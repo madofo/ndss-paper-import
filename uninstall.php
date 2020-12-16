dcc08426d649d66677cd79c3af4a55cf35b33992
Mat Ford <ford@isoc.org>
2020-12-16 16:42:29 +0000

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -9,11 +9,3 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     die;
 }
-
-// Clear database stored data
-
-$papers = get_posts( array( 'post_type' => 'ndss-paper', 'numberposts' => -1 ) );
-
-foreach( $papers as $paper ) {
-    wp_delete_post( $paper->ID, false );
-}
